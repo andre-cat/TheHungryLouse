da3f3c1c8285ec3f48101a5c5d69ba3e711db674
Javier Gonzalez <javiergo.2502@gmail.com>
2023-09-15 02:32:07 -0500

    The both louses are working correctly and the childs are ubicted and working

---
+++ Assets/Scripts/LouseMovement.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LouseMovement : MonoBehaviour
+{
+    [Header("Louse movement settings")] 
+    [SerializeField] [Tooltip("Set the speed of the louse")] 
+    private float moveSpeed;
+    [SerializeField] [Tooltip("Set the rotation speed of the louse")] 
+    private float rotationSpeed;
+    [SerializeField] [Tooltip("Set how high the louse jumps")]
+    private float jumpForce;
+    [Header("Jump sound settings")]
+    [SerializeField] [Tooltip("Is the resource with audio data")]
+    private AudioClip audioFile;
+    [SerializeField] [Tooltip("Is the component that is attached to the GameObject")]
+    private AudioSource audioSource;
+    private Animator louseAnimator;
+    private float inputForwardMovement, inputLateralMovement;
+    private Rigidbody louseRb;
+    private Transform louseBody;
+    private int numberOfJumps = 0;
+    
+    private void Start()
+    {
+        louseRb = GetComponent<Rigidbody>();
+        louseBody = transform.Find("Body");
+        
+        louseAnimator = GetComponent<Animator>();
+        audioSource.clip = audioFile;
+    }
+    private void Update()
+    {
+        inputForwardMovement = Input.GetAxis("Vertical");
+        inputLateralMovement = Input.GetAxis("Horizontal");
+        
+        if (IsOnAChild()) numberOfJumps = 0;
+        if (numberOfJumps < 2) Jump();
+        if (inputForwardMovement != 0 || inputLateralMovement != 0)
+        {
+            MovePlayer();
+            louseAnimator.enabled = true;
+        }
+        else
+        {
+            louseRb.angularVelocity = Vector3.zero;
+            louseAnimator.enabled = false;
+        }
+    }
+
+    private void MovePlayer()
+    {
+        // If The player is in the air, it keeps the vertical movement   
+        Vector3 verticalMove = Vector3.up * louseRb.velocity.y;
+        
+        // Used for focus in the local forward although the louse rotate 
+        Vector3 localDirection = transform.forward;
+        Vector3 localMovement = localDirection * inputForwardMovement * moveSpeed;
+        louseRb.velocity = localMovement + verticalMove;
+
+        float rotation = inputLateralMovement * rotationSpeed * Time.deltaTime;
+        transform.Rotate(Vector3.up * rotation, Space.World);
+    }
+
+    private void Jump()
+    {
+        // Distance in which the body moves to simulate momentum 
+        float impulseOfTheBody = 0.3f;
+        
+        //Only take impulse in the first jump
+        if (Input.GetKeyDown(KeyCode.Space) && numberOfJumps < 1)
+        {
+            louseBody.position -= (Vector3.up * impulseOfTheBody);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {   
+            // Only return the body position in the first jump
+            if (numberOfJumps < 1) 
+            {
+                louseBody.position += (Vector3.up * impulseOfTheBody);
+            }
+            // Overrides the current vertical speed before applying the jump.
+            louseRb.velocity = new Vector3(louseRb.velocity.x, 0, louseRb.velocity.z);
+            louseRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            numberOfJumps += 1;
+            // Jumping sound
+            audioSource.Play();
+        }
+    }
+    // Detect that if the louse is on the head of a child
+    private bool IsOnAChild()
+    {
+        float rayLength = 1.2f;
+        if (Physics.Raycast(transform.position, Vector3.down, out var hit, rayLength))
+        {
+            Vector3 normal = hit.normal;
+            if (normal.y > 0.8f) return true;
+        }
+        return false;
+    }
+}
